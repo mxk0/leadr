a770f02b7ea7266a60db5c110c480751d0d20d76
max <maxwell.a.kramer@gmail.com>
2012-04-01 17:08:43 -0400

    use gunicorn

--- leadr/settings.py
+++ leadr/settings.py
@@ -124,6 +124,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'leadr.browser',
+    'gunicorn',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
