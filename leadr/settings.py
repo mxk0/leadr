2f4a7278de566ba1c974ef21faed83f2cc7b475e
max <maxwell.a.kramer@gmail.com>
2012-04-01 17:25:40 -0400

    templates

--- leadr/settings.py
+++ leadr/settings.py
@@ -50,7 +50,7 @@ LOGIN_URL = '/'
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/Users/MKramer/Desktop/deploy/whereto/leadr/media'
+MEDIA_ROOT = 'leadr/media'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -112,7 +112,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/Users/MKramer/Desktop/deploy/whereto/leadr/templates'
+    'leadr/templates'
 )
 
 INSTALLED_APPS = (
@@ -124,7 +124,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'leadr.browser',
-    'gunicorn',
+    #'gunicorn',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
