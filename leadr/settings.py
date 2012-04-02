dff4dabbc6404fa809b1535067bb20d7b35bf879
max <maxwell.a.kramer@gmail.com>
2012-04-02 00:44:30 -0400

    more responsive

--- leadr/settings.py
+++ leadr/settings.py
@@ -113,8 +113,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    'leadr/templates'
-    #'/Users/MKramer/desktop/deploy/whereto/leadr/templates'
+    'leadr/templates',
+    '/Users/MKramer/desktop/deploy/whereto/leadr/templates',
 )
 
 INSTALLED_APPS = (
