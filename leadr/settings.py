d0eac719211a20226b7afc122926e5236a21f622
max <maxwell.a.kramer@gmail.com>
2012-04-09 21:17:51 -0400

    single_loc modal edits

--- leadr/settings.py
+++ leadr/settings.py
@@ -21,8 +21,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'leadrdb',                      # Or path to database file if using sqlite3.
         'USER': 'MKramer',                      # Not used with sqlite3.
         'PASSWORD': 'maximum1',                  # Not used with sqlite3.
